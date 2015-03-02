54289fdaea30e89dafb66eff0482e17690355d36
Bertocq <bertocq@gmail.com>
2015-03-02 10:35:25 +0100

    Comments to english and refactoring

--- test.rb
+++ test.rb
@@ -3,15 +3,17 @@ require 'nokogiri'
 require 'open-uri'
 require 'json'
 
-#compone un link para obtener el contenido de una charla
-def composeLink (line)
+#compose the link to get the reunion data un markdown
+def compose_link (line)
 	'https://madridrb.jottit.com/' + line.downcase.sub(' ','_') + '?m=edit'
 end
-#devuelve un hash con el nombre y el twitter de un speaker
-def composeSpeaker (speaker)
+
+#returns a hash with the name and twitter url for a given speaker link
+def compose_speaker (speaker)
 	{:name => speaker[/\[?(.*?)\]/,1], :twitter => speaker[/\((.*?)\)?$/,1]}
 end
-#leemos y limpiamos el fichero
+
+#read and clean format of the file
 text=File.open('reunions.txt').read
 text.gsub!(/\r\n?/, "\n")
 text.gsub!(/“|”/,'"')
@@ -22,41 +24,45 @@ reunions = {}
 
 text.each_line do |line|
 	case line
-  	# existe link (miramos que tenga año para no coger links de twitter u otros)
+
+  	#There is a link on the title
   	when /[0-9]{4}\]\(.*?\)/
   		link = line[/[0-9]{4}\]\((.*?)\)/,1]
-  		#si no tiene http delante es un link interno
-  		unless link[/http/,0] then link = composeLink link end
-  	# el link es el titulo
+  		#If there is no 'http' its an internal link
+  		unless link[/http/,0] then link = compose_link link end
+  	#The link is the title itself
   	when /\[\[.*?\]\]/
-  		link = composeLink line[/\[\[(.*?)\]\]/,1]
+  		link = compose_link line[/\[\[(.*?)\]\]/,1]
 	end
 
   	date = line[/\*\*(.*?)\*\*/,1]
 	month= line[/\[(.+) ([0-9]{4})\]/,1].sub("[","")
 	year= line[/\[(.+) ([0-9]{4})\]/,2]
 	title = line[/- \"(.*?)\"/,1]
-	speakers = line[/\", con (.*?)$/,1]
-	if speakers.to_s[/(.*?)\) y \[(.*?)/,0] #si hay varios speakers, los separamos en un array
-		speakers = [composeSpeaker(speakers.to_s[/(.*?)\) y \[(.*?)$/,1]), composeSpeaker(speakers.to_s[/(.*?)\) y \[(.*?)$/,2])]
-	else
-		speakers = [composeSpeaker(speakers.to_s)]
+	speakers = [line[/\", con (.*?)$/,1]]
+
+	#if there is multiple speakers
+	if speakers.to_s[/(.*?)\) y \[(.*?)/,0] 
+		speakers = [speakers.to_s[/(.*?)\) y \[(.*?)$/,1], speakers.to_s[/(.*?)\) y \[(.*?)$/,2]]
 	end
 
-	if link[/madridrb.jottit.com/,0] && link.ascii_only? #si es un link interno (en ascii)
-		#visitamos el link
+	#compose each speakers data
+	speakers.map! {|speaker| compose_speaker(speaker.to_s) } 
+
+	if link[/madridrb.jottit.com/,0] && link.ascii_only? #if its an internal link (en ascii)
+		#visit the url and get the content in markdown
 		doc = Nokogiri::HTML(open(link))
 		content = doc.css('#content_text').map(&:text)
 		
-		#guardamos el contenido
-		File.write("./reunions/#{month}_#{year}.md", content)
+		#write a file for the reunion
 		file = "#{month}_#{year}.md"
+		File.write("./reunions/#{file}", content)
 	end
 
 	reunions[month+"_"+year] = {:link => link, :date => date, :month => month, :year => year, :title => title, :speakers => speakers, :file => file||nil}
 end
 
-#escribimos el fichero con los datos básicos de las reuniones		
+#write json file with all gathered data	
 File.write("./reunions.json", reunions.to_json)
 
 
